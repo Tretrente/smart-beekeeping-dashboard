df98068dd37a1852f7f1da4ba44e3599f6443440
Tretrente <tretrente@gmail.com>
2025-06-02 17:33:26 +0200

    Add initial Spring Boot skeleton with Maven

---
+++ src/main/java/com/tretrente/smart_beekeeping_dashboard/SmartBeekeepingDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.tretrente.smart_beekeeping_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBeekeepingDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBeekeepingDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tretrente/smart_beekeeping_dashboard/SmartBeekeepingDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tretrente.smart_beekeeping_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBeekeepingDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
